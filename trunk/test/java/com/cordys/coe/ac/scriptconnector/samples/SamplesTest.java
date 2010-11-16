2df899a33bbcace7c1e4ef6564da7d537bd26248
srkrishnan@cordys.com <srkrishnan@cordys.com@a8157882-6bf2-5f0d-add7-72afea6e129e>
2010-11-16 11:06:39 +0000

    3 txt files and License headers added.

--- trunk/test/java/com/cordys/coe/ac/scriptconnector/samples/SamplesTest.java
+++ trunk/test/java/com/cordys/coe/ac/scriptconnector/samples/SamplesTest.java
@@ -1,8 +1,19 @@
-/**
- * (c) 2008 Cordys R&D B.V. All rights reserved. The computer program(s) is the
- * proprietary information of Cordys B.V. and provided under the relevant
- * License Agreement containing restrictions on use and disclosure. Use is
- * subject to the License Agreement.
+/*
+ *   Copyright 2004 Cordys R&D B.V. 
+ *
+ *   This file is part of the Cordys Script Connector. 
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 package com.cordys.coe.ac.scriptconnector.samples;
 
